bc7cf50ad1bab39397c22cb2d0b6b6d728148a00
rrTio <tio.rozsraven@ue.edu.ph>
2021-11-17 03:52:00 +0800

    Updated codes: Added Login & Dashboard page and other features

---
+++ dashboard.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Dashboard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+        <link rel="stylesheet" href="css/tailwind/tailwind.min.css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+        <link rel="stylesheet" href="./assets/css/main.css">
+        <link rel="stylesheet" href="./assets/css/dashboard.css">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
+    </head>
+    <body>
+<!-- partial:index.partial.html -->
+<aside>
+    <span class="bckg"></span>
+    <header>
+    <h1>Dashboard</h1>
+    <nav>
+        <ul>
+        <li>
+            <a href="javascript:void(0);" data-title="Summary">Summary</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" data-title="Barangay Officials">Officials</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" data-title="Residents List">Residents List</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" id="registerResident" data-title="Register Resident">Register Resident</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" id="certs" data-title="Certificate Issuance">Ceritificate Issuance</a>
+        </li>
+        <li>
+            <a href="javascript:void(0);" data-title="Search">Search</a>
+        </li>
+        </ul>
+    </nav>
+    </header>
+</aside>
+
+<main>
+  <div class="title">
+    <h2>Welcome</h2>
+    <a href="javascript:void(0);">Hello USERNAME!</a>
+  </div>
+
+  <div class="larg regResident">
+    <div>
+      <h3>Project 1 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 2 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 3 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+
+    <div>
+      <h3>Project 4 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 5 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 6 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+
+    <div>
+      <h3>Project 7 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 8 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 9 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+
+    <div>
+      <h3>Project 10 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 11 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+    <div>
+      <h3>Project 12 <span class="entypo-down-open"></span></h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus iste quia incidunt ad provident ullam quo assumenda expedita quae sapiente ipsa qui esse similique! Modi obcaecati natus sapiente quaerat omnis.</p>
+    </div>
+  </div>
+</main>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script src="./assets/js/dashboard.js"></script>
+
+</body>
+</html>
---
+++ database/database.php
@@ -0,0 +1,10 @@
+<?php
+include_once('connection.php');
+
+if(isset($_POST['btnLogin'])){
+    $adminUsername = $_POST['username'];
+    $adminPassword = $_POST['password'];
+
+    //more codes
+}
+?>
