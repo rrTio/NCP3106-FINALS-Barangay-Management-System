f95dc8cc27bbf42c4a760eb6ec80b9a821e65cec
rrTio <tio.rozsraven@ue.edu.ph>
2021-12-04 00:49:58 +0800

    Removed adding modal

--- residents.php
+++ residents.php
@@ -123,40 +123,6 @@ $position = $_SESSION['position'];
         </table>
       </div>
     </div>
-    <div id="addEmployeeModal" class="modal fade">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <form>
-            <div class="modal-header">
-              <h4 class="modal-title">Add Resident</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            </div>
-            <div class="modal-body">
-              <div class="form-group">
-                <label>Name</label>
-                <input type="text" class="form-control" required>
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input type="email" class="form-control" required>
-              </div>
-              <div class="form-group">
-                <label>Address</label>
-                <textarea class="form-control" required></textarea>
-              </div>
-              <div class="form-group">
-                <label>Phone</label>
-                <input type="text" class="form-control" required>
-              </div>
-            </div>
-            <div class="modal-footer">
-              <input type="button" class="btn btn-default" data-dismiss="modal" value="Cancel">
-              <input type="submit" class="btn btn-success" value="Add">
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
     <!-- Edit Modal HTML -->
     <div id="editEmployeeModal" class="modal fade">
       <div class="modal-dialog">
